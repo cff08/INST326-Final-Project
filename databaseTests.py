de737d3241c0bf0f9ca9f1e24e981ae8a654a906
Crislen <120137120+cff08@users.noreply.github.com>
2025-05-04 13:16:12 -0400

    Add files via upload

---
+++ databaseTests.py
@@ -0,0 +1,26 @@
+import unittest
+from event_database import EventsFinderDB
+from user_database import UsersFinderDB
+
+class TestEvent(unittest.TestCase):
+    def setUp(self):
+        self.db = EventsFinderDB(":memory:")
+
+    def tearDown(self):
+        self.db.close()
+
+    def test_add_event(self):
+        self.db.add_events("School Event", "2025-04-10", "HBK", "2:00pm - 4:00pm") 
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.db = UsersFinderDB(":memory:")
+
+    def tearDown(self):
+        self.db.close()  
+
+    def test_add_user(self):
+        self.db.add_user("John", "john@example.com", "123456789")      
+
+if __name__ == "__main__":
+    unittest.main()
