d41fae62ff316f251a11428e98a59db300334fb5
ighosh13 <ighosh13@terpmail.umd.edu>
2025-05-09 00:46:54 -0400

    cleaned up implementation of main to incorporate all scripts

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from system import StorageSystem
 from event_database import EventsFinderDB
 from user_database import UsersFinderDB
 from scraper import EventScraper  # assuming this is the class name
+from notification_system import send_event_notifications
 
 def main():
     """
@@ -26,18 +27,26 @@ def main():
     db = EventsFinderDB()
     storage = StorageSystem()
     user_db = UsersFinderDB()
-    notifier = NotificationSystem()
+   
 
     event_list = scraper.scrape_events()
     
     for event in event_list:
         title = event['title']
         date = event['date']
-        location = "UMD Campus"
-        event_time = "TBD"
+    
+        if not event_exists(title, date):
+            event_id = db.add_events(title, date)
+            print(f"Inserted: {title} - {date}")
+        
+        else:
+            print(f"Skipped: {title} already exists")
+    
+    send_event_notifications(db.conn)
     
     db.close()
-    
+    storage.close()
+    user_db.close()
 
 
    
